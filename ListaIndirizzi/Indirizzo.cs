8849fecb6f2a39da52e512625999a19e14bd49eb
colombovf <colombovf@gmail.com>
2022-12-12 18:18:58 +0100

    Fine Esercizio

---
+++ ListaIndirizzi/Indirizzo.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaIndirizzi
+{
+    public class Indirizzo
+    {
+        //ATTRIBUTI
+        private string name;
+        private string surname;
+        private string street;
+        private string city;
+        private string province;
+        private string zip;
+        //STATO
+        //COSTRUTTORI
+        public Indirizzo()
+        {
+
+        }
+
+        public Indirizzo(string name, string surname, string street, string city, string province, string zip)
+        {
+            this.name = name;
+            this.surname = surname;
+            this.street = street;
+            this.city = city;
+            this.province = province;
+            this.zip = zip;
+        }
+        //GETTER
+        public string getName()
+        {
+            return name;
+        }
+        public string getSurname()
+        {
+            return surname;
+        }
+        public string getStreet()
+        {
+            return street;
+        }
+        public string getCity()
+        {
+            return city;
+        }
+        public string getProvince()
+        {
+            return province;
+        }
+        public string getZip()
+        {
+            return zip;
+        }
+        //SETTER
+        //METODI
+        public override string ToString()
+        {
+            string obj = name + surname + street + city + province + zip;
+            return obj;
+        }
+    }
+}
--- ListaIndirizzi/Program.cs
+++ ListaIndirizzi/Program.cs
@@ -1,1 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+using ListaIndirizzi;
+
+List<Indirizzo> personalData = new List<Indirizzo>();
+
+try
+{
+    StreamReader file = File.OpenText("C:/Users/Samsung/Desktop/GenItaly/Esercizi/EX-VS2022/ListaIndirizzi/ListaIndirizzi/addresses.csv");
+    while (!file.EndOfStream)
+    {
+        string row = file.ReadLine();
+        string[] datainfo = row.Split(",");
+
+        Indirizzo currentAddress = new Indirizzo(datainfo[0], datainfo[1], datainfo[2], datainfo[3], datainfo[4], datainfo[5]);
+        Console.WriteLine(currentAddress.ToString());
+        personalData.Add(currentAddress);
+    } 
+
+} catch (Exception e)
+{
+    Console.WriteLine("Errore");
+}
